3bee30e89215dc3cd46a74dfe80d82acac0100b6
João Virgilio <jvirgiliomartins09@gmail.com>
2020-12-02 00:49:48 -0300

    Inicio, criação da classe produto

--- pessoa.py
+++ pessoa.py
@@ -1,6 +1,6 @@
 class Pessoa:
 	
-	def __init__():
+	def __init__(self):
 		self._nome = None
 		self._cpf = None
 		self._endereco = None
@@ -9,14 +9,24 @@ class Pessoa:
 	def nome(self):
 		return self._nome
 
-	@setter.nome
+	@nome.setter
 	def nome(self, nome):
 		self._nome = nome
-
 
 	@property
 	def cpf(self):
 		return self._cpf
-		
-		
 
+	@cpf.setter
+	def cpf(self, cpf):
+		self._cpf = cpf
+
+	@property
+	def endereco(self):
+		return self._endereco
+
+
+	@endereco.setter
+	def endereco(self, endereco):
+		self._endereco = endereco
+	
---
+++ produto.py
@@ -0,0 +1,57 @@
+class Produto:
+	
+	def __init__(self):
+		self._codigo = 0
+		self._nome = None
+		self._valor = 0
+		self._data_validade = None
+
+
+	@property
+	def codigo(self):
+		return self._codigo
+
+	@codigo.setter
+	def codigo(self, codigo):
+		self._codigo = codigo
+
+
+	@property
+	def nome(self):
+		return self._nome
+
+	@nome.setter
+	def nome(self, nome):
+		self._nome = nome
+
+	@property
+	def valor(self):
+		return self._valor
+
+	@valor.setter
+	def valor(self):
+		if(valor >= 0):
+			self._valor = valor
+		
+		else:
+			print("Valor invalido\n")
+
+
+	@property
+	def data_validade(self):
+		return self._data_validade
+
+	
+	@data_validade.setter
+	def data_validade(self, data_validade):
+		self._data_validade = data_validade
+
+	
+		
+
+	
+	
+
+	
+		
+		
