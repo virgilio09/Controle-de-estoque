f6ededbce87915584f1743bee0065028455f8fd7
João Virgilio <jvirgiliomartins09@gmail.com>
2020-12-01 22:41:53 -0300

    Inicio, criação da classe pessoa

---
+++ pessoa.py
@@ -0,0 +1,22 @@
+class Pessoa:
+	
+	def __init__():
+		self._nome = None
+		self._cpf = None
+		self._endereco = None
+
+	@property
+	def nome(self):
+		return self._nome
+
+	@setter.nome
+	def nome(self, nome):
+		self._nome = nome
+
+
+	@property
+	def cpf(self):
+		return self._cpf
+		
+		
+
