0ce98ac0e91edd0c37929a5daf09bb3b8605287e
João Virgilio <jvirgiliomartins09@gmail.com>
2020-12-04 12:15:56 -0300

    Metodos, adicionados na classe cliete

--- cliente.py
+++ cliente.py
@@ -6,7 +6,19 @@ class Cliente(Pessoa):
 		super().__init__()
 		self._carrinho = [] 
 
-
 	@property
 	def carrinho(self):
 		return self._carrinho
+
+
+	# adicionar ao carrinho
+	def add_produto(self):
+		pass
+
+	# remover do carrinho
+	def rm_produto(self):
+		pass
+
+
+	def total_carrinho(self)
+		pass
