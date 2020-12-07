27c6eb556d37935c1b8f79b40c7a651d7097507c
Somar07 <raafao07@gmail.com>
2020-12-06 23:08:08 -0300

    Implementação da função add_carrinho da classe Cliente

--- cliente.py
+++ cliente.py
@@ -1,10 +1,14 @@
 from pessoa import Pessoa
+from estoque import Estoque
 
 class Cliente(Pessoa): 
 	
+	_total_compras = 0
+
 	def __init__(self):
 		super().__init__()
-		self._carrinho = [] 
+		self._carrinho = []
+
 
 	@property
 	def carrinho(self):
@@ -12,13 +16,20 @@ class Cliente(Pessoa):
 
 
 	# adicionar ao carrinho
-	def add_produto(self):
-		pass
+	def add_produto(self,Estoque):
+		produto = Estoque.buscar_produto()
+		if(produto != []):
+			self._carrinho.append(produto[0])
+			print("Produto adicionado com sucesso!")
+			return 1
+		else:
+			print("Produto não encontrado no estoque!")
+			return 0
 
 	# remover do carrinho
 	def rm_produto(self):
 		pass
 
 
-	def total_carrinho(self)
+	def total_carrinho(self):
 		pass
