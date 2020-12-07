762817cb056579780208b749f8a10372ba09dba9
Somar07 <raafao07@gmail.com>
2020-12-07 17:24:00 -0300

    Criação das funcões menu_cliente, menu_func,remover, cadastro,listar

--- menus.py
+++ menus.py
@@ -19,13 +19,64 @@ def menu_func():
     print("|     Menu Funcionario  |")
     print("-"*25)
     print("|                       |")
-    print("| 1 - Cadastrar  Produto|")
+    print("| 1 - Cadastros         |")
+    print("|                       |")
+    print("| 2 - Remover           |")
+    print("|                       |")
+    print("| 3 - Analise           |")
+    print("|                       |")
+    print("| 4 - Listar            |")
+    print("|                       |")
+    print("| 0 - Voltar            |")
+    print("-"*25)
+
+    opcao = input("Escolha uma opcão: ")
+    return opcao
+
+def menu_cadastro():
+    print("-"*25)
+    print("|     Menu Funcionario  |")
+    print("-"*25)
+    print("|                       |")
+    print("| 1 - Cadastrar Produto |")
     print("|                       |")
     print("| 2 - Cadastrar Cliente |")
     print("|                       |")
-    print("| 3 - Remover Produto   |")
+    print("| 3 - Cadastrar Func.   |")
     print("|                       |")
-    print("| 4 - Remover Cliente   |")
+    print("| 0 - Voltar            |")
+    print("-"*25)
+
+    opcao = input("Escolha uma opcão: ")
+    return opcao
+
+def menu_remover():
+    print("-"*25)
+    print("|     Menu Funcionario  |")
+    print("-"*25)
+    print("|                       |")
+    print("| 1 - Remover Produto   |")
+    print("|                       |")
+    print("| 2 - Remover Cliente   |")
+    print("|                       |")
+    print("| 3 - Remover Func.     |")
+    print("|                       |")
+    print("| 0 - Voltar            |")
+    print("-"*25)
+
+    opcao = input("Escolha uma opcão: ")
+    return opcao
+
+def menu_listar():
+    print("-"*25)
+    print("|     Menu Funcionario  |")
+    print("-"*25)
+    print("|                       |")
+    print("| 1 - Listar Estoque    |")
+    print("|                       |")
+    print("| 2 - Listar Func       |")
+    print("|                       |")
+    print("| 3 - Listar Clientes   |")
     print("|                       |")
     print("| 0 - Voltar            |")
     print("-"*25)
