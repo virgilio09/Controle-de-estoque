ca798e19fb16edbd59db2ed59925aa576d45efc9
Somar07 <raafao07@gmail.com>
2020-12-07 16:33:38 -0300

    modulo para exibição de menus que forem usados pelo programa

---
+++ menus.py
@@ -0,0 +1,34 @@
+def menu_cliente():
+    print("-"*25)
+    print("|     Menu Cliente      |")
+    print("-"*25)
+    print("|                       |")
+    print("| 1 - Realizar Compra   |")
+    print("|                       |")
+    print("| 2 - Consultar Valor   |")
+    print("|                       |")
+    print("| 0 - Voltar            |")
+    print("-"*25)
+
+    opcao = input("Escolha uma opcão: ")
+    return opcao
+
+
+def menu_func():
+    print("-"*25)
+    print("|     Menu Funcionario  |")
+    print("-"*25)
+    print("|                       |")
+    print("| 1 - Cadastrar  Produto|")
+    print("|                       |")
+    print("| 2 - Cadastrar Cliente |")
+    print("|                       |")
+    print("| 3 - Remover Produto   |")
+    print("|                       |")
+    print("| 4 - Remover Cliente   |")
+    print("|                       |")
+    print("| 0 - Voltar            |")
+    print("-"*25)
+
+    opcao = input("Escolha uma opcão: ")
+    return opcao
