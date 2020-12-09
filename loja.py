76fa746b0bd624885243090c02ebcba1cf76d261
Somar07 <raafao07@gmail.com>
2020-12-09 09:52:49 -0300

    Implementando funcoes, remover(cliente e funcionario), mostra(cliente e funcionario)

--- loja.py
+++ loja.py
@@ -45,11 +45,55 @@ class Loja():
 		return resultado
 	
 	def buscar_cliente(self):
-		cpf = input("Digite seu cpf: ")
+		cpf = input("\nDigite cpf do Cliente: ")
 		resultado = [cliente for cliente in self._list_clientes if cliente.cpf == cpf]
 		return resultado
 	
+	def remove_cliente(self):
+		
+		result = self.buscar_cliente()
+
+		if(result != []):
+			self._list_clientes.remove(result[0])
+			print("Cliente removido com sucesso!\n")
+		else:
+			print("Cliente não cadastrado!\n")
+			
+
+	def remove_func(self):
+		result = self.buscar_funcionario()
+
+		if(result != []):
+			self._list_funcionarios.remove(result[0])
+			print("Funcionario removido com sucesso!\n")
+		else:
+			print("Funcionario não cadastrado!\n")
+
+	def mostrar_clientes(self):
+		if(self._list_clientes != []):
+			print("---- Lista de Clientes ----")
+			for cliente in self._list_clientes:
+				print("Nome: {}".format(cliente.nome))
+				print("Cpf: {}".format(cliente.cpf))
+				print("-"*20)
+		else:
+			print("Cadastro vazio!")
+
+	def mostrar_func(self):
+		if(self._list_funcionarios != []):
+			print("---- Lista de Funcionarios ----")
+			for func in self._list_funcionarios:
+				print("\nNome: {}".format(func.nome))
+				print("Cpf: {}".format(func.cpf))
+				print("Salario: {}".format(func.salario))
+				print("-"*20)
+		else:
+			print("Cadastro vazio!")
+
+
 	def analise_compras(self, Cliente):
+		
+		
 		if(Cliente.carrinho != []):
 			self.cupom_fiscal(Cliente)
 			op = input("\nDeseja aprovar esta compra?(S/N)").lower()
@@ -61,6 +105,7 @@ class Loja():
 					print("\nCpf incorreto!\n")
 			else:
 				print("Compra cancelada!")
+		
 
 	def cupom_fiscal(self, Cliente):
 	
